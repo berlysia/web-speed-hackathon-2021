3725daa3699f4ccb4229dcf08f74c612cd5cc825
berlysia <berlysia@gmail.com>
2021-12-11 15:18:07 +0900

    bye lodash

--- client/src/components/foundation/SoundWaveSVG/SoundWaveSVG.jsx
+++ client/src/components/foundation/SoundWaveSVG/SoundWaveSVG.jsx
@@ -1,5 +1,33 @@
-import _ from 'lodash';
 import React from 'react';
+
+function zip(a, b) {
+  // サイズは同じ前提でチェック飛ばす
+  const result = [];
+  const len = a.length;
+  for (let i = 0; i < len; ++i) {
+    result.push([a[i], b[i]]);
+  }
+
+  return result;
+}
+
+function chunk(arr, size) {
+  const result = [];
+
+  for (let count = 0; count < arr.length; count += size) {
+    result.push(arr.slice(count, count + size));
+  }
+
+  return result;
+}
+
+function mean(arr) {
+  let sum = 0;
+  for (const item of arr) {
+    sum += item;
+  }
+  return sum / arr.length;
+}
 
 /**
  * @param {ArrayBuffer} data
@@ -14,18 +42,18 @@ async function calculate(data) {
     audioCtx.decodeAudioData(data.slice(0), resolve, reject);
   });
   // 左の音声データの絶対値を取る
-  const leftData = _.map(buffer.getChannelData(0), Math.abs);
+  const leftData = buffer.getChannelData(0).map((x) => Math.abs(x));
   // 右の音声データの絶対値を取る
-  const rightData = _.map(buffer.getChannelData(1), Math.abs);
+  const rightData = buffer.getChannelData(1).map((x) => Math.abs(x));
 
   // 左右の音声データの平均を取る
-  const normalized = _.map(_.zip(leftData, rightData), _.mean);
+  const normalized = zip(leftData, rightData).map((x) => mean(x));
   // 100 個の chunk に分ける
-  const chunks = _.chunk(normalized, Math.ceil(normalized.length / 100));
+  const chunks = chunk(normalized, Math.ceil(normalized.length / 100));
   // chunk ごとに平均を取る
-  const peaks = _.map(chunks, _.mean);
+  const peaks = chunks.map((x) => mean(x));
   // chunk の平均の中から最大値を取る
-  const max = _.max(peaks);
+  const max = Math.max(...peaks);
 
   return { max, peaks };
 }

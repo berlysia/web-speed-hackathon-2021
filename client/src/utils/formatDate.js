23cb53886ebeadd0a8b3ee0f789a8ada5fb3f74c
berlysia <berlysia@gmail.com>
2022-01-02 00:25:41 +0900

    date-fnsのimport方法の誤りを修正

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -1,7 +1,7 @@
 import ja from 'date-fns/locale/ja';
-import { format } from 'date-fns/format';
-import { parseISO } from 'date-fns/parseISO';
-import { formatISO } from 'date-fns/formatISO';
+import format from 'date-fns/esm/format';
+import parseISO from 'date-fns/esm/parseISO';
+import formatISO from 'date-fns/esm/formatISO';
 
 export function formatDate(date) {
   return format(parseISO(date), 'YoModo', { locale: ja });
